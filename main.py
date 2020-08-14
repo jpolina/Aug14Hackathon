e2f317fd73b0dad75700ca9d8062ddd56483cbff
UnloadingGnat <jaavinjm123@gmail.com>
2020-08-14 14:25:02 -0400

    hi

---
+++ main.py
@@ -0,0 +1,57 @@
+## Pygame Base 
+##
+# @author UnloadingGnat
+# @date 14/08/2020
+
+#
+
+## Pygame setup
+import pygame
+pygame.init()
+size = (800, 600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Hackathon")
+
+## MODEL - Data use in system
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+# Loop until the user clicks the close button.
+done = False
+c = 10
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+pos = 500
+
+x= 500
+y= 200
+
+## Main Program Loop
+while not done:
+    ## CONTROL
+    # Check for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        print(event)
+    # Game logic
+    pos = pos - 5
+    ## VIEW
+    # Clear screen
+    screen.fill(BLUE)
+
+    # Draw
+
+    pygame.draw.ellipse(screen,WHITE,[x,y,100,100])
+
+
+    # Update Screen
+    pygame.display.flip()
+    clock.tick(60)
+
+# Close the window and quit
+pygame.quit()
